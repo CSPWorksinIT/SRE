fe305df44c5002430dd4ba5523bca58c2a6e791f
CSPallapu <cspallapu@gmail.com>
2026-04-19 17:14:37 +0530

    SRE APP V:1.0.4 built

--- sre-chaos-lab/app/app.py
+++ sre-chaos-lab/app/app.py
@@ -1,14 +1,22 @@
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
 
+REQUEST_COUNT = Counter('request_count', 'Total Requests')
+
 @app.route("/")
 def home():
+    REQUEST_COUNT.inc()
     return "SRE App Running 🚀"
 
 @app.route("/health")
 def health():
     return "OK", 200
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype='text/plain')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
